fd3bf001fa8a841cf2ed4f91285951c75bbf8a90
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2015-09-13 23:02:08 -0300

    Add online since command

--- botling.rb
+++ botling.rb
@@ -1,6 +1,7 @@
 require 'telegram/bot'
 
 bot_name = "Botling (Telegram Delivery Bot version - Alpha)"
+online_at = Time.now
 
 Telegram::Bot::Client.run(ENV['BOT_TOKEN']) do |bot|
 	bot.listen do |message|
@@ -9,6 +10,8 @@ Telegram::Bot::Client.run(ENV['BOT_TOKEN']) do |bot|
 				bot.api.send_message(chat_id: message.chat.id, text: "Bem vindo ao #{bot_name}!\nUse /ajuda pra saber os comandos disponíveis!")
 			when '/ajuda'
 				bot.api.send_message(chat_id: message.chat.id, text: "#{bot_name}")
+			when '/online'
+				bot.api.send_message(chat_id: message.chat.id, text: "Tô aqui online desde #{online_at.strftime("%H:%M de %d/%m/%Y")}.")
 		end
 	end
 end
